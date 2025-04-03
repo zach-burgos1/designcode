5949baa42c3ef8bf4719f17b1d3b13b59b8a2e48
Ruth <ruth_edwards1@baylor.edu>
2025-04-03 16:26:45 -0500

    just edited pin numbers for testing linear actuator

--- TestComponents/TestLinearActuator/TestLinearActuator.ino
+++ TestComponents/TestLinearActuator/TestLinearActuator.ino
@@ -7,31 +7,38 @@
  */
 
 // constants won't change
-const int ENA_PIN = 9; // the Arduino pin connected to the EN1 pin L298N
-const int IN1_PIN = 6; // the Arduino pin connected to the IN1 pin L298N
-const int IN2_PIN = 5; // the Arduino pin connected to the IN2 pin L298N
+// const int ENA_PIN = 9; // the Arduino pin connected to the EN1 pin L298N
+const int IN1_PIN = 8; // the Arduino pin connected to the IN1 pin L298N
+const int IN2_PIN = 9; // the Arduino pin connected to the IN2 pin L298N
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pins as outputs.
-  pinMode(ENA_PIN, OUTPUT);
+  // pinMode(ENA_PIN, OUTPUT);
+  pinMode(3, OUTPUT); 
+  digitalWrite(3, 0); 
+
   pinMode(IN1_PIN, OUTPUT);
   pinMode(IN2_PIN, OUTPUT);
 
-  digitalWrite(ENA_PIN, HIGH);
+  // digitalWrite(ENA_PIN, HIGH);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   // extend the actuator
-  digitalWrite(IN1_PIN, HIGH);
-  digitalWrite(IN2_PIN, LOW);
 
-  delay(5000); // actuator will stop extending automatically when reaching the limit
 
   // retracts the actuator
   digitalWrite(IN1_PIN, LOW);
   digitalWrite(IN2_PIN, HIGH);
 
   delay(5000); // actuator will stop retracting automatically when reaching the limit
+
+  digitalWrite(IN1_PIN, HIGH);
+  digitalWrite(IN2_PIN, LOW);
+
+  delay(5000); // actuator will stop extending automatically when reaching the limit
+  delay(5000); 
+
 }
