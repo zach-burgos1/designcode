3f583f7615d52973d110fac5d669f9a3c1bbc9e5
Ruth <ruth_edwards1@baylor.edu>
2025-04-10 15:15:41 -0500

    added definitions of l298n pins (no pin #'s yet)

---
+++ BoxOpsCode/definitions.h
@@ -0,0 +1,40 @@
+
+
+
+
+// STEPPERS
+const int STEPS_PER_REV = 400
+
+// PINS
+// MOTORS PINS
+const int PIN_L1_EN1 = -1;
+const int PIN_L1_EN2 = -1;
+const int PIN_L1_IN1 =  8;
+const int PIN_L1_IN2 =  9;
+const int PIN_L1_IN3 = 10;
+const int PIN_L1_IN4 = 11;
+
+const int PIN_L2_EN1 = -1;
+const int PIN_L2_EN2 = -1;
+const int PIN_L2_IN1 = -1;
+const int PIN_L2_IN2 = -1;
+const int PIN_L2_IN3 = -1;
+const int PIN_L2_IN4 = -1;
+
+const int PIN_L3_EN1 = -1;
+const int PIN_L3_EN2 = -1;
+const int PIN_L3_IN1 = -1;
+const int PIN_L3_IN2 = -1;
+const int PIN_L3_IN3 = -1;
+const int PIN_L3_IN4 = -1;
+
+const int PIN_L4_EN1 = -1;
+const int PIN_L4_EN2 = -1;
+const int PIN_L4_IN1 = -1;
+const int PIN_L4_IN2 = -1;
+const int PIN_L4_IN3 = -1;
+const int PIN_L4_IN4 = -1;
+
+const int PIN_RELAY = -1;
+
+// SENSOR
