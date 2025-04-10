f13539abdbd99262ecbaa3721212731cb763d4be
Ruth <ruth_edwards1@baylor.edu>
2025-04-10 15:16:18 -0500

    Added init function for l298n

---
+++ BoxOpsCode/l298n_functions.ino
@@ -0,0 +1,29 @@
+#include "definitions.h"
+
+void initL298NS(){
+  pinMode(PIN_L1_EN1, OUTPUT);
+  pinMode(PIN_L1_EN2, OUTPUT);
+  pinMode(PIN_L1_IN1, OUTPUT);
+  pinMode(PIN_L1_IN2, OUTPUT);
+  pinMode(PIN_L1_IN3, OUTPUT);
+  pinMode(PIN_L1_IN4, OUTPUT);
+
+  pinMode(PIN_L2_EN1, OUTPUT);
+  pinMode(PIN_L2_EN2, OUTPUT);
+  pinMode(PIN_L2_IN1, OUTPUT);
+  pinMode(PIN_L2_IN2, OUTPUT);
+  pinMode(PIN_L2_IN3, OUTPUT);
+  pinMode(PIN_L2_IN4, OUTPUT);
+
+  pinMode(PIN_L3_EN1, OUTPUT);
+  pinMode(PIN_L3_EN2, OUTPUT);
+  pinMode(PIN_L3_IN1, OUTPUT);
+  pinMode(PIN_L3_IN2, OUTPUT);
+  pinMode(PIN_L3_IN3, OUTPUT);
+  pinMode(PIN_L3_IN4, OUTPUT);
+
+  pinMode(PIN_L4_EN1, OUTPUT);
+  pinMode(PIN_L4_IN1, OUTPUT);
+  pinMode(PIN_L4_IN2, OUTPUT);  
+}
+
