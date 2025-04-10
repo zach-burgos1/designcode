a38651616c7538294f65d6687a8c32c6470febab
Ruth <ruth_edwards1@baylor.edu>
2025-04-10 15:15:10 -0500

    Added really rough skeleton of main function

--- BoxOpsCode/BoxOpsCode.ino
+++ BoxOpsCode/BoxOpsCode.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "definitions.h"
 
+void setup() {
+  // Setup Steppers
+  // Setup Linear Actuator
+  // Setup Relay / Flip Relay
+  // 
+
+  // initialize digital pins as outputs.
+  pinMode(ENA_PIN, OUTPUT);
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+
+  digitalWrite(ENA_PIN, HIGH);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+  // extend the actuator
+  digitalWrite(IN1_PIN, HIGH);
+  digitalWrite(IN2_PIN, LOW);
 
+  delay(5000); // actuator will stop extending automatically when reaching the limit
+
+  // retracts the actuator
+  digitalWrite(IN1_PIN, LOW);
+  digitalWrite(IN2_PIN, HIGH);
+
+  delay(5000); // actuator will stop retracting automatically when reaching the limit
 }
