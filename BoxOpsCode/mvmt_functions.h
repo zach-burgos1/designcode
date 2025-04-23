3bd96523566d52fc2b2f68667b578a12e53b5bc7
Ruth <ruth_edwards1@baylor.edu>
2025-04-23 18:30:07 -0500

    Added movement functions, moved steppers and servos into defintitions instead, wrote gantry up and down

--- BoxOpsCode/mvmt_functions.h
+++ BoxOpsCode/mvmt_functions.h
@@ -2,8 +2,20 @@
 #define MVMT_FUNCTIONS_H
 
 #include "definitions.h"
+#include <Stepper.h>
+#include <Servo.h>
+
 
 void retractLinAct();
 void extendLinAct();
+
+void gantryUp(); 
+void gantryDown();
+
+void clawOpen(); 
+void clawClose(); 
+
+void caseExport(); 
+void caseImport(); 
 
 #endif // MVMT_FUNCTIONS_H
--- BoxOpsCode/mvmt_functions.ino
+++ BoxOpsCode/mvmt_functions.ino
@@ -19,3 +19,48 @@ void extendLinAct(){
   digitalWrite(PIN_L4_EN2, LOW); 
   
 }
+
+void gantryUp(){
+  digitalWrite(PIN_L2_EN1, HIGH);
+  digitalWrite(PIN_L2_EN2, HIGH);
+
+  int timeStart = millis(); 
+  int timeNow = millis(); 
+
+  while(timeNow - timeStart <= VERT_DELAY){
+    stepGantryV.step(2*STEPS_PER_REV); 
+    timeNow = millis(); 
+  }
+
+  digitalWrite(PIN_L2_EN1, LOW); 
+  digitalWrite(PIN_L2_EN2, LOW); 
+}
+void gantryDown(){
+  digitalWrite(PIN_L2_EN1, HIGH);
+  digitalWrite(PIN_L2_EN2, HIGH);
+
+  int timeStart = millis(); 
+  int timeNow = millis(); 
+
+  while(timeNow - timeStart <= VERT_DELAY){
+    stepGantryV.step(-2*STEPS_PER_REV); 
+    timeNow = millis(); 
+  }
+
+  digitalWrite(PIN_L2_EN1, LOW); 
+  digitalWrite(PIN_L2_EN2, LOW); 
+}
+
+void clawOpen(){
+
+}
+void clawClose(){
+
+}
+
+void caseExport(){
+
+}
+void caseImport(){
+
+}
