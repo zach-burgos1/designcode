d30fde558fc9d546f362a00dc930bb80c56a36d6
Zach <zach_burgos1@baylor.edu>
2025-04-24 03:40:11 -0500

    refactored and changed all the delays

--- BoxOpsCode/BoxOpsCode.ino
+++ BoxOpsCode/BoxOpsCode.ino
@@ -22,134 +22,61 @@ void setup() {
   initServos(); 
   clawOpen(); 
   retractLinAct();
-  // clawClose(); 
-  // retractLinAct(); 
-  // return; 
-
-  // Enable the Case Driver
-  // digitalWrite(PIN_L3_EN1, HIGH); 
-  // digitalWrite(PIN_L3_EN2, HIGH); 
-
-  // pinMode(PIN_RELAY, OUTPUT);
-  // digitalWrite(PIN_RELAY, HIGH);
-
-  Serial.begin(9600);  
-
-  // Initialize timer
-//  stateStart = millis();
-
-  digitalWrite(PIN_L1_EN1, HIGH);
-  digitalWrite(PIN_L1_EN2, HIGH);
 }
 
 void loop() {
-  /*gantryUp();
-  gantryPush(); 
-
-  clawClose();
-
-  gantryDown(); 
-  gantryUp(); 
-
-  gantryPull(); 
-
-  return; 
-
-  stepGantryH.step(-3.9 * STEPS_PER_REV);
-  delay(500); 
-
-  stepGantryH.step(3.9*STEPS_PER_REV); 
-  delay(500);
-
-  return; 
-
-  gantryPush(); 
-  delay(1000); 
-  gantryPull();
-  delay(1000); 
-  return; 
-
-  caseForward();
-  delay(1000); 
-  caseBackward(); 
-  delay(1000);
-
-  return;
-
-/*  gantryPush();
-  delay(2000);
-  gantryPull(); 
-  delay(2000); *//*
-  gantryUp();
-  delay(1000); 
-  clawSemiClose();
-  delay(1000); 
-  gantryDown();
-  delay(1000);
-  clawClose();
-  delay(1000);
-  gantryUp();
-  delay(1000);
-  gantryDown();
-  delay(1000);    
-
-//  gantryDown();
-//  delay(1000);
-//  gantryUp();
-//  delay(1000);
-  return;*/
 
   conveyorBeltOn();
   // RELAY
   sensorState = digitalRead(PIN_BB);
   if (!sensorState && lastState){
     count++; //Counts Products
-    delay(1000);
+    delay(250);
     conveyorBeltOff();
-    delay(4000);
+    if(count == 1){
+      delay(4000);
+    }
     
   }
   lastState = sensorState;
   if (count == 2 && !action) {
-    delay(1000);
+    delay(250);
     digitalWrite(PIN_RELAY, LOW);
-    delay(1000);
+    delay(250);
     extendLinAct();
     retractLinAct();
     digitalWrite(PIN_RELAY, HIGH);
     action = true;  //allows the LA to only work once while waiting for next product to load
   }
   if (count == 4){ //Grabs Products before the LA and resets basically every four.
-    delay(1000);
+    delay(250);
     partExtLinAct();
-    delay(1000);
+    delay(250);
 
     gantryUp();
-    delay(1000); 
+    delay(250); 
     clawSemiCloseLoad();
-    delay(1000); 
+    delay(250); 
     gantryDown();
-    delay(1000);
+    delay(250);
     clawClose();
-    delay(1000);
+    delay(250);
     gantryUp();
-    delay(1000);
+    delay(250);
     gantryPush(); 
     delay(1000); 
     gantryDownCase(); 
-    delay(1000); 
+    delay(250); 
     clawSemiCloseCase(); 
-    delay(1000);
+    delay(250);
     gantryUpCase(); 
-    delay(1000);
+    delay(250);
     gantryPull(); 
-    delay(1000); 
+    delay(250); 
     clawOpen(); 
-    delay(1000); 
+    delay(250); 
     gantryDown(); 
-
-    // gantryDown();
-    delay(1000);
+    delay(250);
 
     count = 0;
     countphase++; 
@@ -157,15 +84,10 @@ void loop() {
   }
   if(countphase == 2){
     caseForward();
-    delay(2000); 
+    delay(250); 
     caseBackward(); 
-    delay(2000); 
+    delay(250); 
     countphase = 0; 
   }
-
-  
-
-  // SERVO
-  
 }
 
